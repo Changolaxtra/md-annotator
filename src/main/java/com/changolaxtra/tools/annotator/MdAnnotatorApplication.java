e2b5810eb1f769bd8da81658638460fdbccb9571
Changolaxtra <dan.rojasx@gmail.com>
2025-07-30 16:19:30 -0600

    feat: initial commit

---
+++ src/main/java/com/changolaxtra/tools/annotator/MdAnnotatorApplication.java
@@ -0,0 +1,13 @@
+package com.changolaxtra.tools.annotator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdAnnotatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdAnnotatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changolaxtra/tools/annotator/MdAnnotatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changolaxtra.tools.annotator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdAnnotatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
